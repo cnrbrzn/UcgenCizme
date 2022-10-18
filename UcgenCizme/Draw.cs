ed759d1e1598526b457a99ed7559ee9a94c8011d
Caner Borazan <caner.borazan@lunatr.com>
2022-10-18 15:00:33 +0300

    Add project files.

---
+++ UcgenCizme/Draw.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UcgenCizme
+{
+    public class Draw
+    {
+        public void Triangle(int triangleLength)
+        {
+            for (int i = 1; i <= triangleLength; i++)
+            {
+                for (int j = 0; j < triangleLength - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 0; k < 2 * i - 1; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ UcgenCizme/Program.cs
@@ -0,0 +1,5 @@
+﻿using UcgenCizme;
+
+int triangleLength = Convert.ToInt32(Console.ReadLine());
+Draw d = new Draw();
+d.Triangle(triangleLength);
